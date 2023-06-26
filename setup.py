14e39f289c32044303a6bfe0c1c8964a4789214d
dungpq <dungphanquang@vccorp.vn>
2023-06-26 16:20:50 +0700

    update setup

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(name='clean_architecture',
       version='0.0.1',
       description='This package has shared components.',
       author='Dung Phan Quang',
       author_email='phanquangdng@gmail.com',
-      packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
       license='LICENSE.txt',
     )
